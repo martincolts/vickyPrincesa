64e2a9cea4ae544e68a655ffc940cdb33ee62444
tincho <martinlopez89.quequen@gmail.com>
2018-05-08 23:38:32 -0300

    sadad

---
+++ src/main/java/com/example/vickyPrincesa/VickyPrincesaApplication.java
@@ -0,0 +1,12 @@
+package com.example.vickyPrincesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VickyPrincesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VickyPrincesaApplication.class, args);
+	}
+}
